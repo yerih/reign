823a188152158233054d8eb1ae5caf543c5328e7
yerih 3d link <yerih@3dlinkweb.com>
2022-09-21 10:07:03 -0400

    Se agrega navigation

--- app/src/main/java/com/admissions/empty_project/presentation/main/MainFragment.kt
+++ app/src/main/java/com/admissions/empty_project/presentation/main/MainFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.admissions.domain.HackerNew
 import com.admissions.empty_project.data.R
 import com.admissions.empty_project.data.databinding.FragmentMainBinding
@@ -13,14 +14,14 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainFragment : Fragment(R.layout.fragment_main) {
 
-    private val viewModel: MainViewModel by viewModels()
+    private val mViewModel: MainViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val adapter = HackerNewsAdapter{}
-        adapter.submitList(listOf(HackerNew("hola", "", "como", "estas"),
-            HackerNew("hola", "", "como", "estas"),
-            HackerNew("hola", "", "como", "estas")))
+        val adapter = HackerNewsAdapter{ findNavController().navigate(MainFragmentDirections.actionMainFragmentToDetailFragment())}
+        adapter.submitList(listOf(
+            HackerNew(), HackerNew(),HackerNew(), HackerNew()
+        ))
         val binding = FragmentMainBinding.bind(view).apply {
             recycler.adapter = adapter
         }
--- domain/src/main/java/com/admissions/domain/HackerNew.kt
+++ domain/src/main/java/com/admissions/domain/HackerNew.kt
@@ -1,8 +1,8 @@
 package com.admissions.domain
 
 data class HackerNew(
-    val title: String,
-    val url: String,
-    val author: String,
-    val date: String,
+    val title: String = "title",
+    val url: String = "https://www.reuters.com/world/europe/putin-signs-decree-\u003cem\u003emobili\u003c/em\u003esation-says-west-wants-destroy-russia-2022-09-21/",
+    val author: String = "author",
+    val date: String = "date",
 )
